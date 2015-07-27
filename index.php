9694a3ce8d7576c533d7f7ac7d8cac4a72da04a3
Lucho Web <luchoweb.com@gmail.com>
2015-07-27 09:53:11 -0500

    Bootstrap y otros

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<link href="images/favicon.png" rel="shortcut icon" type="image/png" />
+	<meta charsef="utf-8" />
+	<title>PagaDiario</title>
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/styles.css" />
+</head>
+<!-- Powered by LUCHOWEB.COM -->
+<body>
+	<header>
+		<figure>
+			<img src="images/logo.png" id="logo" />
+		</figure>
+	</header>
+	<section></section>
+	<footer>
+		<p>
+			&copy; 2008-<?php echo date('Y'); ?>
+		</p>
+	</footer>
+</body>
+</html>
